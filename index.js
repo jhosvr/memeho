2d972a44969d89163f98a6c504e34c0a7fbcb73b
Justin Ho <justinho@hosvr.com>
2019-01-16 05:55:17 -0500

    reordering some of the functions

--- index.js
+++ index.js
@@ -1,51 +1,42 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js')
 const bot = new Discord.Client();
 
-const branch = process.env.BRANCH_NAME
-const token = process.env.DBOT_TOKEN
+const branch = process.env.BRANCH_NAME;
+const token = process.env.DBOT_TOKEN;
 
-process_name = 'dbot-' + branch
-process.title = process_name
+process_name = 'dbot-' + branch;
+process.title = process_name;
 
-var prefix = ':'
+const prefix = ':';
+
+bot.on('error', console.error);
+bot.login(token);
 
 bot.on('message', function(message){
+
+    /* Command responses */
+    if (message.content.startsWith(prefix + "STATUS")){
+      // always true
+      message.channel.send('Memeho is currently active!');
+    }
+
+    /* Eavesdrop responses: reading users messages */
     var words = message.content.toUpperCase().split(' ');
-
-    if(words == 'HELLO')
-	  {
+    if(words == 'HELLO'){
         message.reply('howdy');
-    };
+    }
 
-    if(words.includes('PRINT')
-    {
+    if(words.includes('PRINT')){
       message.channel.send(words);
     }
 
-    if(message.content == 'TOM')
-    {
+    if(message.content == 'TOM'){
         // tag tom
         message.channel.send('<@127856466571821056>, mei takes no skill. ');
     }
-
-    /*
-    COMMANDS THAT START WITH PREFIX ':'
-    todo:
-        convert to case/switch statements
-        probably use ':' commands in a seperate file and source them
-    */
-
-	  if (message.content.startsWith(prefix + "STATUS"))
-	  {
-		    // always true
-		    message.channel.send('Memeho is currently active!');
-	  };
 
 });
 
 bot.on('ready', function(){
     console.log('Bot has been initiated');
-})
-
-bot.on('error', console.error);
-bot.login(token);
+});
