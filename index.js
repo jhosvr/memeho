b289ff0f9dd2fb7567f57a15e4d02c20612fa2b6
Justin Ho <justinho@hosvr.com>
2019-01-16 10:07:51 -0500

    reorder some code, add if/else so prevent unecesesary processing

--- index.js
+++ index.js
@@ -7,36 +7,36 @@ const token = process.env.DBOT_TOKEN;
 process_name = 'dbot-' + branch;
 process.title = process_name;
 
-const prefix = ':';
-
-bot.on('error', console.error);
 bot.login(token);
+bot.on('error', console.error);
+bot.on('ready', function(){
+    console.log('Bot has been initiated');
+});
+
+const prefix = ':';
 
 bot.on('message', function(message){
     /* Command responses */
     if (message.content.toUpperCase().startsWith(prefix + "STATUS")){
       // always true
       message.channel.send('Memeho is currently active!');
-    }
+    } else {
 
-    /* Eavesdrop responses: reading users messages */
-    var words = message.content.toLowerCase().split(' ');
-    if(words.includes('hello')){
+      /* Eavesdrop responses: reading users messages */
+      var words = message.content.toLowerCase().split(' ');
+      if(words.includes('hello')){
         message.reply('howdy');
-    }
+      }
 
-    if(words.includes('widowmaker') || words.includes('widow')){
-      // tag Jason
-      message.channel.send('<@219523329483210752> oh, is this your last game?');
-    }
+      if(words.includes('widowmaker') || words.includes('widow')){
+        // tag Jason
+        message.channel.send('<@219523329483210752> oh, is this your last game?');
+      }
 
-    if(words.includes('tom')){
+      if(words.includes('tom')){
         // tag tom
         message.channel.send('<@127856466571821056>, mei takes no skill. ');
+      }
+
     }
-
-});
-
-bot.on('ready', function(){
-    console.log('Bot has been initiated');
-});
+  });
