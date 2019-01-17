cb174b70783ac4cba2a5825f0c87932cffd3ba95
Justin Ho <justinho@hosvr.com>
2019-01-17 17:26:20 -0500

    update

--- overwatch.js
+++ overwatch.js
@@ -78,16 +78,3 @@ module.exports = (message, callback) => {
   });
 
 };
-© 2019 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
-Press h to open a hovercard with more details.
