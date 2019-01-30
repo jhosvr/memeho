9a3f495f8bbbdc3b9ec439533df29e21f6f6a8fb
Justin Ho <justinho@hosvr.com>
2019-01-30 00:58:23 -0500

    update to copy other working code

--- commands/overwatch.js
+++ commands/overwatch.js
@@ -15,74 +15,81 @@ let buildAPIUrl = (battleTag) => {
 };
 
 module.exports = (message, callback) => {
-  name: 'overwatch',
-  aliases: ['ow'],
-  description: 'overwatch api',
-  args: true,
-  usage: 'battle#tag',
-  guildOnly: false,
-  cooldown: 3,
 
-  execute(message, callback) {
-    let msgParts = message.content.split(' ');
+  let msgParts = message.split(' ');
 
-    let battleTag = null;
-    platform = platformOptions[0];
-    region = regionOptions[0];
+  let battleTag = null;
+  platform = platformOptions[0];
+  region = regionOptions[0];
 
-    // Part user's options
-    msgParts.forEach( (item) => {
-      if (item.indexOf('#') > -1) {
-        battleTag = item.replace('#', '-');
-      } else if (item.indexOf('platform=') > -1) {
-        platform = item.split('=').pop();
-      } else if (item.indexOf('region=') > -1) {
-        region = item.split('=').pop();
-      }
-    });
-
-    // Check user's options for validity
-    if (!battleTag) {
-      return callback('Invalid BattleTag', 'Please provide a valid BattleTag. Ex: User#1234')
-    } else {
-      if (platformOptions.indexOf(platform) == -1)
-        return callback('Invalid Platform', 'Platform invalid, allowed options are: ' + platformOptions.join(' '));
-      if (regionOptions.indexOf(region) == -1)
-        return callback('Invalid Region', 'Region invalid, allowed options are: ' + regionOptions.join(' '));
+  // Part user's options
+  msgParts.forEach( (item) => {
+    if (item.indexOf('#') > -1) {
+      battleTag = item.replace('#', '-');
+    } else if (item.indexOf('platform=') > -1) {
+      platform = item.split('=').pop();
+    } else if (item.indexOf('region=') > -1) {
+      region = item.split('=').pop();
     }
+  });
 
-    let url = buildAPIUrl(battleTag);
-    console.log('Getting stats from URL: ' + url);
+  // Check user's options for validity
+  if (!battleTag) {
+    return callback('Invalid BattleTag', 'Please provide a valid BattleTag. Ex: User#1234')
+  } else {
+    if (platformOptions.indexOf(platform) == -1)
+      return callback('Invalid Platform', 'Platform invalid, allowed options are: ' + platformOptions.join(' '));
+    if (regionOptions.indexOf(region) == -1)
+      return callback('Invalid Region', 'Region invalid, allowed options are: ' + regionOptions.join(' '));
+  }
 
-    request(url, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        // For API errors, send back the error to the user
-        let statsJSON = JSON.parse(body);
-        if (statsJSON.statusCode == 404)
-          return callback('API Error: ' + statsJSON.statusCode + ' - ' + statsJSON.error, statsJSON.error);
+  let url = buildAPIUrl(battleTag);
+  console.log('Getting stats from URL: ' + url);
 
-        // Stats were returned okay, let's parse them
-        let stats = statsJSON;
-        let statLevel = stats.level;
-        let statCompRank = stats.competitive.rank;
-        let statCompRecord = stats.games.competitive.wins + 'W';
-        let statCompWinrate = (stats.games.competitive.wins / stats.games.competitive.played * 100).toFixed(2) + '%';
-        let statCompTime = stats.playtime.competitive;
-        let statQuickWins = stats.games.quickplay.wins;
-        let statQuickTime = stats.playtime.quickplay;
+  request(url, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      // For API errors, send back the error to the user
+      let statsJSON = JSON.parse(body);
+      if (statsJSON.statusCode == 404)
+        return callback('API Error: ' + statsJSON.statusCode + ' - ' + statsJSON.error, statsJSON.error);
 
-        // Format the string we are going to return to the user
-        let statsString = '';
-        statsString += '**' + stats.username + '**\n';
-        statsString += 'Level: ' + statLevel + ' | Competitive Rank: ' + statCompRank + '\n';
-        statsString += 'Competitive Record (Current season): **' + statCompRecord + '** | *' + statCompWinrate + '* winrate\n';
-        statsString += '**' + statQuickWins + '** Quick Play wins\n';
-        statsString += '**' + statCompTime + '** Competitive | ' + '**' + statQuickTime + '** Quick Play'
+      // Stats were returned okay, let's parse them
+      let stats = statsJSON;
+      let statLevel = stats.level;
+      let statPortrait = stats.portrait;
+      let statCompRank = stats.competitive.rank;
+      let statCompRecord = stats.games.competitive.won + 'W';
+      let statCompWon = stats.games.competitive.lost +'L';
+      let statCompDraw = stats.games.competitive.draw + 'D';
+      let statCompWinrate = (stats.games.competitive.won / stats.games.competitive.played * 100).toFixed(2) + '%';
+      let statCompTime = stats.playtime.competitive;
+      let statQuickplayWins = stats.games.quickplay.won;
+      let statQuickplayLost = stats.games.quickplay.lost;
+      let statQuickTime = stats.playtime.quickplay;
+      let statlevelframe = stats.levelFrame;
+      let statRankImg = stats.competitive.rank_img;
+     // let statRankImg2 = (+ 'https://d1u1mce87gyfbn.cloudfront.net/game/rank-icons/season-2/rank-1.png' + = ':BRONZE:'; + 'https://d1u1mce87gyfbn.cloudfront.net/game/rank-icons/season-2/rank-2.png'+ = ':SILVER:'; +'https://d1u1mce87gyfbn.cloudfront.net/game/rank-icons/season-2/rank-3.png' + = ':GOLD:')'';
+      let statRankImg2 = stats.competitive.rank_img;
+      let statStar = stats.star;
 
-        return callback(null, statsString);
-      } else {
-        return callback(error || response.statusCode, null);
-      }
-    });
-  },
+      // Format the string we are going to return to the user
+
+ let statsString = '';
+    // statsString += '```';
+      statsString += '**' + stats.username + '**\n';
+      statsString += 'Portrait : **' + stats.portrait + '**\n';
+      statsString += 'Niveau : **' + statLevel + '** | Image de la Bordure du Niveau :**' + stats.levelFrame + '** \n';
+      statsString += 'Image du Prestige : **' + stats.star + '**\n';
+      statsString += 'Classement compétitif: **' + statCompRank + '** | Image du Grade : ' + statRankImg2 + ' **' + stats.competitive.rank_img + '**\n';
+      statsString += 'Victoire en partie rapide : **' + statQuickplayWins + ' victoires !**\n';
+      statsString += 'Infos compétitif (saison en cours) : **' + stats.games.competitive.played + ' jouer !** | Détail : **' + stats.games.competitive.won+ ' victoires !** | ** ' + stats.games.competitive.lost+ ' défaites !** | ** ' + stats.games.competitive.draw+ ' égalités !** | ** ' + statCompWinrate+ ' Winrate ! ** \n';
+      statsString += '**' + statCompTime + '** jouer en Partie Competitive | ' + '**' + statQuickTime + '** jouer en Partie rapide'
+     // statsString += '```';
+
+      return callback(null, statsString);
+    } else {
+      return callback(error || response.statusCode, null);
+    }
+  });
+
 };
