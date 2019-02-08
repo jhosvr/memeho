7b0e51bcd0a70410522961e1fa37e5e5953fb360
Justin Ho <justinho@hosvr.com>
2019-02-07 22:59:23 -0500

    initial commit

---
+++ commands/afk.js
@@ -0,0 +1,17 @@
+module.exports = {
+	name: 'afk',
+  aliases: ['',''],
+	description: 'kicks an adk user out of voice chat',
+  args: true, // true: args are needed
+  usage: '<user>',
+  guildOnly: true, // false: bot can accept commands via DM
+  cooldown: 3, // sec to wait until user can use again
+
+	execute(message, args) {
+    let alluser = guild.members;
+    let afkuser = args[0];
+
+		message.channel.send(allusers);
+    message.channel.reply(afkuser);
+	},
+};
